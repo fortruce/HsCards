a3aed6909857356f5c2fb88f95f54fc30503eefe
Joseph Rollins <rollins.joseph@gmail.com>
2015-08-02 12:14:48 -0400

    onSearch spawn the searchCards action and list card results under search

--- src/frontend/components/pages/CardSearch.js
+++ src/frontend/components/pages/CardSearch.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Search from '../atoms/Search.js';
+import { searchCards } from '../../actions/cards';
 
 @connect(state => ({
-  cards: state.cards
+  cards: state.cards.cards
 }))
 export default class CardSearch extends React.Component {
+  onSearch = (search) => {
+    this.props.dispatch(searchCards(search));
+  }
+
   render() {
     return (
       <div>
         <h1>Search</h1>
-        <Search />
+        <Search
+          onSearch={ this.onSearch } />
+        <ul>
+          { this.props.cards.map(c => <li>{ c.name }</li>) }
+        </ul>
       </div>
     );
   }
