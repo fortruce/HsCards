d72bda35d31096da6e94625333b78b9d49943262
Joseph Rollins <rollins.joseph@gmail.com>
2015-08-03 22:24:07 -0400

    add example implementation of wiring up a toggleable filter to react router query params

--- src/frontend/components/pages/Explore.js
+++ src/frontend/components/pages/Explore.js
@@ -1,13 +1,16 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import assign from 'object-assign';
 
+import ToggleFilter from '../atoms/ToggleFilter';
 import Search from '../atoms/Search.js';
 import * as searchActions from '../../actions/search';
 
 @connect(state => ({
   cards: state.search.cards,
-  search: state.search.search
+  search: state.search.search,
+  filters: state.search.filters
 }))
 export default class Explore extends React.Component {
   static contextTypes = {
@@ -15,7 +18,7 @@ export default class Explore extends React.Component {
   }
 
   onSearch = (search) => {
-    this.context.router.transitionTo(`/search/${search}`);
+    this.context.router.transitionTo(`/search/${search}`, this.props.location.query);
   }
 
   onSearchChange = (search) => {
@@ -32,6 +35,19 @@ export default class Explore extends React.Component {
           onSearch={ this.onSearch }
           value={ this.props.search }
           onChange={ this.onSearchChange } />
+        <ToggleFilter
+          label='Druid'
+          toggled={ !!(this.props.location.query && this.props.location.query['druid']) }
+          onToggle={ (toggled) => {
+            const ident = 'druid';
+            let { query, pathname } = this.props.location;
+            query = assign({}, query);
+            if (toggled)
+              query = assign(query, { [ident]: 1 });
+            else
+              delete query[ident];
+            this.context.router.transitionTo(pathname, query);
+          }} />
         { this.props.children &&
             React.cloneElement(this.props.children, { actions, ...this.props }) }
       </div>
