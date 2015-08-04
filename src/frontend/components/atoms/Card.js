70c17d27441b0c6f348a19d618689887df77e6c2
Joseph Rollins <rollins.joseph@gmail.com>
2015-08-03 21:10:18 -0400

    add size prop to load different sized image from wowhead

--- src/frontend/components/atoms/Card.js
+++ src/frontend/components/atoms/Card.js
@@ -1,20 +1,23 @@
 import React, { PropTypes } from 'react';
 
-function cardImgUrl(id) {
-  return `http://wow.zamimg.com/images/hearthstone/cards/enus/original/${id}.png`;
+function cardImgUrl(id, size = 'original') {
+  return `http://wow.zamimg.com/images/hearthstone/cards/enus/${size}/${id}.png`;
 }
 
 export default class Card extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired,
+    size: PropTypes.oneOf(['original', 'medium', 'small'])
   }
 
   render() {
     const { name, id } = this.props;
     return (
       <div>
-        <img src={ cardImgUrl(id) } alt={ name } />
+        <img
+            src={ cardImgUrl(id, this.props.size) }
+            alt={ name } />
       </div>
     );
   }
