0d18001d2a961528fadc8873a170521f3147e892
fortruce <>
2015-08-04 16:12:38 -0400

    add decorator that stores component state in route query params

---
+++ src/frontend/decorators/urlStorage.js
@@ -0,0 +1,51 @@
+import React, { PropTypes } from 'react';
+import assign from 'object-assign';
+
+//
+// urlStorage decorates React components to store their state in the route
+//
+//  storageKey: String that represents the query parameter that should
+//    be used to store the data in the route
+//  serializeFn: Fn (Any) -> String
+//    Must serialize the Component's data into a string to store in query params
+//  deserializeFn: Fn (String) -> Any
+//    Must deserialize the query param string back into data format for Component
+//
+//  Deserialized data is passed down to Component thru this.props.data
+//  Component must use this.serialize to indicate state changes (a la this.setState)
+//
+
+export default function urlStorage(storageKey, serializeFn, deserializeFn) {
+  return DecoratedComponent => {
+    class UrlStorageComponent extends React.Component {
+      static contextTypes = {
+        router: PropTypes.object.isRequired
+      }
+
+      render() {
+        const { query } = this.props.location;
+        const data = deserializeFn((query && query[storageKey]) || '');
+        return (
+          <DecoratedComponent
+            data={ data }
+            {...this.props} />
+        );
+      }
+    }
+
+    DecoratedComponent.contextTypes = assign(
+      {},
+      DecoratedComponent.contextTypes,
+      { router: PropTypes.object.isRequired }
+    );
+
+    DecoratedComponent.prototype.serialize = function(data) {
+      this.context.router.transitionTo(
+        this.props.location.pathname,
+        assign({}, this.props.location.query, { [storageKey]: serializeFn(data) })
+      );
+    }
+
+    return UrlStorageComponent;
+  }
+}
